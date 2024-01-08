e7b98b8dcfe12facbb3f7fc7167b28e0abaac61b
Asapanna Rakesh <rakeshark22@gmail.com>
2024-01-08 17:58:23 +0530

    import statement not working

--- random_headers/_modidx.py
+++ random_headers/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://INF800.github.io',
                 'git_url': 'https://github.com/INF800/random-headers',
                 'lib_path': 'random_headers'},
-  'syms': {'random_headers.core': {'random_headers.core.foo': ('core.html#foo', 'random_headers/core.py')}}}
+  'syms': { 'random_headers.core': {'random_headers.core.foo': ('core.html#foo', 'random_headers/core.py')},
+            'random_headers.static_headers': { 'random_headers.static_headers.get_header': ( 'static_headers.html#get_header',
+                                                                                             'random_headers/static_headers.py')}}}
